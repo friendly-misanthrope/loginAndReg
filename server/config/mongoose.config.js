eaca91879b3246c18d2dc4305f3aa3bb4b66ac6e
NickBarkerDev <nickthedevguy@gmail.com>
2023-07-13 22:25:54 -0700

    initial commit, server

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+// * Don't forget to change DB name!
+
+// ! ATTN: Please change connection URI below to 127.0.0.1
+// ! before running server.
+
+// ! I run VSCode in a virutal Linux environment that requires
+// ! the use a local network IP.
+
+mongoose.connect('mongodb://192.168.1.3:27017/login_reg_users', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Database connection established')
+})
+.catch((err) => {
+  console.log("Connection to database failed", err)
+})
